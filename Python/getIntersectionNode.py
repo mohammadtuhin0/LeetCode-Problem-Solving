657b6303c5f9956f7a6d1b2bfbfbe6d5e6590e18
mohammadtuhin0 <md.tuhin42525@gmail.com>
2026-04-06 19:16:18 +0600

    160. Intersection of Two Linked Lists

---
+++ Python/getIntersectionNode.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA, headB):
+        p1 = headA
+        p2 = headB
+
+        c = 0
+
+        while True:
+            if p1 == p2:
+                return p1
+
+            p1 = p1.next
+            p2 = p2.next
+
+            if p2 == None:
+                c+=1
+                p2 = headA
+            if p1 == None:
+                p1 = headB
+            
+            if c>1:
+                return None
